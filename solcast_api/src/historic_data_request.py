d2058ef7c4324b43eaeffdfb49d135e3d7e718ed
gavinkit <kitchingg15@gmail.com>
2022-11-04 13:35:15 +0000

    MetOffice API working first version - not getting irradiance

--- solcast_api/src/historic_data_request.py
+++ solcast_api/src/historic_data_request.py
@@ -2,8 +2,8 @@ import requests
 
 key = "o2QrwWjwvAIaBQ4O4ZnfaHfT5dHXf34b"
 #url = f"https://api.solcast.com.au/data/live/radiation_and_weather?latitude=-33.856784&longitude=151.215297&output_parameters=air_temp,dni,ghi&api_key={key}"
-url = f"https://api.solcast.com.au/data/forecast/radiation_and_weather?latitude=-33.856784&longitude=151.215297&output_parameters=air_temp,dni,ghi&format=json&api_key={key}"
-#url = f"https://api.solcast.com.au/world_radiation/estimated_actuals?latitude=-33.856784&longitude=151.215297&hours=168&api_key={key}"
+#url = f"https://api.solcast.com.au/data/forecast/radiation_and_weather?latitude=-33.856784&longitude=151.215297&output_parameters=air_temp,dni,ghi&format=json&api_key={key}"
+url = f"https://api.solcast.com.au/world_radiation/estimated_actuals?latitude=-33.856784&longitude=151.215297&hours=168&api_key={key}"
 
 payload={}
 headers = {
